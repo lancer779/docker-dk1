3f1834af1530ed0a045dd5b20a57e3e3417e3b2f
lancer779 <lancer779@disroot.org>
2024-12-07 09:09:25 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,409 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const os = require('os');
+const fs = require("fs");
+const path = require("path");
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
+const { execSync } = require('child_process');
+const FILE_PATH = process.env.FILE_PATH || './temp';
+const projectPageURL = process.env.URL || '';
+const intervalInseconds = process.env.TIME || 120;
+const UUID = process.env.UUID || '89c13786-25aa-4520-b2e7-12cd60fb5202';
+const JOS_SERVER = process.env.JOS_SERVER || 'nz.abc.cn';
+const JOS_PORT = process.env.JOS_PORT || '5555';
+const JOS_KEY = process.env.JOS_KEY || '';
+const FAGO_DOTDAY = process.env.FAGO_DOTDAY || 'beyobk.clisaxlo.eu.org';
+const FAGO_AUTH = process.env.FAGO_AUTH || 'eyJhIjoiMDk0OTIxZDhmY2JmMWI1ODk4NjVmMDhhNDAxYzBhZGUiLCJ0IjoiNmJjMDFmZGMtNjUzYS00ZDI2LWE0MWYtNzk1NzNmMzI2OTBiIiwicyI6Ik9XSTRNVFU1WmpVdFlUYzBZaTAwTXpkaExXRTFaakF0WlRBNVl6bGlNbUkzTXpWaSJ9';
+const CFIP = process.env.CFIP || 'na.ma';
+const CFPORT = process.env.CFPORT || 443;
+const NAME = process.env.NAME || 'Vlsgo';
+const FAGO_PORT = process.env.FAGO_PORT || 27080;
+const PORT = process.env.SERVER_PORT || process.env.PORT || 35100;
+
+
+if (!fs.existsSync(FILE_PATH)) {
+  fs.mkdirSync(FILE_PATH);
+  console.log(`${FILE_PATH} is created`);
+} else {
+  console.log(`${FILE_PATH} already exists`);
+}
+
+
+const pathsToDelete = [ 'saber', 'bsk', 'caster', 'sub.txt', 'boot.log'];
+function cleanupOldFiles() {
+  pathsToDelete.forEach((file) => {
+    const filePath = path.join(FILE_PATH, file);
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        console.error(`Skip Delete ${filePath}`);
+      } else {
+        console.log(`${filePath} deleted`);
+      }
+    });
+  });
+}
+cleanupOldFiles();
+
+
+app.get("/", function(req, res) {
+  res.send("The heart of freedom!");
+});
+
+
+const config = {
+  log: { access: '/dev/null', error: '/dev/null', loglevel: 'none' },
+  inbounds: [
+    { port: FAGO_PORT, protocol: 'vless', settings: { clients: [{ id: UUID, flow: 'xtls-rprx-vision' }], decryption: 'none', fallbacks: [{ dest: 45101 }, { path: "/vless451", dest: 45102 }, { path: "/vmess451", dest: 45103 }, { path: "/trojan451", dest: 45104 }] }, streamSettings: { network: 'tcp' } },
+    { port: 45101, listen: "127.0.0.1", protocol: "vless", settings: { clients: [{ id: UUID }], decryption: "none" }, streamSettings: { network: "ws", security: "none" } },
+    { port: 45102, listen: "127.0.0.1", protocol: "vless", settings: { clients: [{ id: UUID, level: 0 }], decryption: "none" }, streamSettings: { network: "ws", security: "none", wsSettings: { path: "/vless451" } }, sniffing: { disable: false, destOverride: ["http", "tls", "quic"], metadataOnly: false } },
+    { port: 45103, listen: "127.0.0.1", protocol: "vmess", settings: { clients: [{ id: UUID, alterId: 0 }] }, streamSettings: { network: "ws", wsSettings: { path: "/vmess451" } }, sniffing: { disable: false, destOverride: ["http", "tls", "quic"], metadataOnly: false } },
+    { port: 45104, listen: "127.0.0.1", protocol: "trojan", settings: { clients: [{ password: UUID }] }, streamSettings: { network: "ws", security: "none", wsSettings: { path: "/trojan451" } }, sniffing: { disable: false, destOverride: ["http", "tls", "quic"], metadataOnly: false } },
+  ],
+  dns: { servers: ["https+local://8.8.8.8/dns-query"] },
+  outbounds: [
+    { protocol: "freedom" },
+    {
+      tag: "WARP",
+      protocol: "wireguard",
+      settings: {
+        secretKey: "YFYOAdbw1bKTHlNNi+aEjBM3BO7unuFC5rOkMRAz9XY=",
+        address: ["172.16.0.2/32", "2606:4700:110:8a36:df92:102a:9602:fa18/128"],
+        peers: [{ publicKey: "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=", allowedIPs: ["0.0.0.0/0", "::/0"], endpoint: "162.159.193.10:2408" }],
+        reserved: [78, 135, 76],
+        mtu: 1280,
+      },
+    },
+  ],
+  routing: { domainStrategy: "AsIs", rules: [{ type: "field", domain: ["domain:openai.com", "domain:ai.com"], outboundTag: "WARP" }] },
+};
+fs.writeFileSync(path.join(FILE_PATH, 'config.json'), JSON.stringify(config, null, 2));
+
+
+function getSystemArchitecture() {
+  const arch = os.arch();
+  if (arch === 'arm' || arch === 'arm64' || arch === 'aarch64') {
+    return 'arm';
+  } else {
+    return 'amd';
+  }
+}
+
+
+function downloadFile(fileName, fileUrl, callback) {
+  const filePath = path.join(FILE_PATH, fileName);
+  const writer = fs.createWriteStream(filePath);
+
+  axios({
+    method: 'get',
+    url: fileUrl,
+    responseType: 'stream',
+  })
+    .then(response => {
+      response.data.pipe(writer);
+
+      writer.on('finish', () => {
+        writer.close();
+        console.log(`Download ${fileName} successfully`);
+        callback(null, fileName);
+      });
+
+      writer.on('error', err => {
+        fs.unlink(filePath, () => { });
+        const errorMessage = `Download ${fileName} failed: ${err.message}`;
+        console.error(errorMessage);
+        callback(errorMessage);
+      });
+    })
+    .catch(err => {
+      const errorMessage = `Download ${fileName} failed: ${err.message}`;
+      console.error(errorMessage);
+      callback(errorMessage);
+    });
+}
+
+
+async function downloadFilesAndRun() {
+  const architecture = getSystemArchitecture();
+  const filesToDownload = getFilesForArchitecture(architecture);
+
+  if (filesToDownload.length === 0) {
+    console.log(`Can't find a file for the current architecture`);
+    return;
+  }
+
+  const downloadPromises = filesToDownload.map(fileInfo => {
+    return new Promise((resolve, reject) => {
+      downloadFile(fileInfo.fileName, fileInfo.fileUrl, (err, fileName) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(fileName);
+        }
+      });
+    });
+  });
+
+  try {
+    await Promise.all(downloadPromises);
+  } catch (err) {
+    console.error('Error downloading files:', err);
+    return;
+  }
+
+
+  function authorizeFiles(filePaths) {
+    const newPermissions = 0o775;
+
+    filePaths.forEach(relativeFilePath => {
+      const absoluteFilePath = path.join(FILE_PATH, relativeFilePath);
+
+      fs.chmod(absoluteFilePath, newPermissions, (err) => {
+        if (err) {
+          console.error(`Empowerment failed for ${absoluteFilePath}: ${err}`);
+        } else {
+          console.log(`Empowerment success for ${absoluteFilePath}: ${newPermissions.toString(8)}`);
+        }
+      });
+    });
+  }
+  const filesToAuthorize = ['./caster', './saber', './bsk'];
+  authorizeFiles(filesToAuthorize);
+
+
+  let JOS_TLS = '';
+  if (JOS_SERVER && JOS_PORT && JOS_KEY) {
+    const tlsPorts = ['443', '8443', '2096', '2087', '2083', '2053'];
+    if (tlsPorts.includes(JOS_PORT)) {
+      JOS_TLS = '--tls';
+    } else {
+      JOS_TLS = '';
+    }
+    const command = `nohup ${FILE_PATH}/caster -s ${JOS_SERVER}:${JOS_PORT} -p ${JOS_KEY} ${JOS_TLS} >/dev/null 2>&1 &`;
+    try {
+      await exec(command);
+      console.log('caster is running');
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    } catch (error) {
+      console.error(`caster running error: ${error}`);
+    }
+  } else {
+    console.log('JOS variable is empty,skip running');
+  }
+
+
+  const command1 = `nohup ${FILE_PATH}/saber -c ${FILE_PATH}/config.json >/dev/null 2>&1 &`;
+  try {
+    await exec(command1);
+    console.log('saber is running');
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  } catch (error) {
+    console.error(`saber running error: ${error}`);
+  }
+
+
+  if (fs.existsSync(path.join(FILE_PATH, 'bsk'))) {
+    let args;
+
+    if (FAGO_AUTH.match(/^[A-Z0-9a-z=]{120,250}$/)) {
+      args = `tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token ${FAGO_AUTH}`;
+    } else if (FAGO_AUTH.match(/TunnelSecret/)) {
+      args = `tunnel --edge-ip-version auto --config ${FILE_PATH}/tunnel.yml run`;
+    } else {
+      args = `tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile ${FILE_PATH}/boot.log --loglevel info --url http://localhost:${FAGO_PORT}`;
+    }
+
+    try {
+      await exec(`nohup ${FILE_PATH}/bsk ${args} >/dev/null 2>&1 &`);
+      console.log('bsk is running');
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+    } catch (error) {
+      console.error(`Error executing command: ${error}`);
+    }
+  }
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
+}
+
+function getFilesForArchitecture(architecture) {
+  if (architecture === 'arm') {
+    return [
+      { fileName: "caster", fileUrl: "https://github.com/eooce/test/releases/download/ARM/swith" },
+      { fileName: "saber", fileUrl: "https://github.com/eooce/test/releases/download/ARM/web" },
+      { fileName: "bsk", fileUrl: "https://github.com/eooce/test/releases/download/arm64/bot13" },
+    ];
+  } else if (architecture === 'amd') {
+    return [
+      { fileName: "caster", fileUrl: "https://github.com/eooce/test/raw/main/amd64" },
+      { fileName: "saber", fileUrl: "https://github.com/eooce/test/raw/main/web" },
+      { fileName: "bsk", fileUrl: "https://github.com/eooce/test/raw/main/bot13" },
+    ];
+  }
+  return [];
+}
+
+
+function argoType() {
+  if (!FAGO_AUTH || !FAGO_DOTDAY) {
+    console.log("FAGO_DOTDAY or FAGO_AUTH variable is empty, use quick tunnels");
+    return;
+  }
+
+  if (FAGO_AUTH.includes('TunnelSecret')) {
+    fs.writeFileSync(path.join(FILE_PATH, 'tunnel.json'), FAGO_AUTH);
+    const tunnelYaml = `
+  tunnel: ${FAGO_AUTH.split('"')[11]}
+  credentials-file: ${path.join(FILE_PATH, 'tunnel.json')}
+  protocol: http2
+  
+  ingress:
+    - hostname: ${FAGO_DOTDAY}
+      service: http://localhost:${FAGO_PORT}
+      originRequest:
+        noTLSVerify: true
+    - service: http_status:404
+  `;
+    fs.writeFileSync(path.join(FILE_PATH, 'tunnel.yml'), tunnelYaml);
+  } else {
+    console.log("FAGO_AUTH mismatch TunnelSecret,use token connect to tunnel");
+  }
+}
+argoType();
+
+
+async function extractDomains() {
+  let argoDomain;
+
+  if (FAGO_AUTH && FAGO_DOTDAY) {
+    argoDomain = FAGO_DOTDAY;
+    console.log('FAGO_DOTDAY:', argoDomain);
+    await generateLinks(argoDomain);
+  } else {
+    try {
+      const fileContent = fs.readFileSync(path.join(FILE_PATH, 'boot.log'), 'utf-8');
+      const lines = fileContent.split('\n');
+      const argoDomains = [];
+      lines.forEach((line) => {
+        const domainMatch = line.match(/https?:\/\/([^ ]*trycloudflare\.com)\/?/);
+        if (domainMatch) {
+          const domain = domainMatch[1];
+          argoDomains.push(domain);
+        }
+      });
+
+      if (argoDomains.length > 0) {
+        argoDomain = argoDomains[0];
+        console.log('ArgoDomain:', argoDomain);
+        await generateLinks(argoDomain);
+      } else {
+        console.log('ArgoDomain not found, re-running bsk to obtain ArgoDomain');
+
+        fs.unlinkSync(path.join(FILE_PATH, 'boot.log'));
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        const args = `tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile ${FILE_PATH}/boot.log --loglevel info --url http://localhost:${FAGO_PORT}`;
+        try {
+          await exec(`nohup ${path.join(FILE_PATH, 'bsk')} ${args} >/dev/null 2>&1 &`);
+          console.log('bsk is running.');
+          await new Promise((resolve) => setTimeout(resolve, 3000));
+          await extractDomains();
+        } catch (error) {
+          console.error(`Error executing command: ${error}`);
+        }
+      }
+    } catch (error) {
+      console.error('Error reading boot.log:', error);
+    }
+  }
+
+
+  async function generateLinks(argoDomain) {
+    const metaInfo = execSync(
+      'curl -s https://speed.cloudflare.com/meta | awk -F\\" \'{print $26"-"$18}\' | sed -e \'s/ /_/g\'',
+      { encoding: 'utf-8' }
+    );
+    const ISP = metaInfo.trim();
+
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const VMESS = { v: '2', ps: `${NAME}-${ISP}`, add: CFIP, port: CFPORT, id: UUID, aid: '0', scy: 'none', net: 'ws', type: 'none', host: argoDomain, path: '/vmess451?ed=2560', tls: 'tls', sni: argoDomain, alpn: '' };
+        const subTxt = `
+vless://${UUID}@${CFIP}:${CFPORT}?encryption=none&security=tls&sni=${argoDomain}&type=ws&host=${argoDomain}&path=%2Fvless451%3Fed%3D2560#${NAME}-${ISP}
+  
+vmess://${Buffer.from(JSON.stringify(VMESS)).toString('base64')}
+  
+trojan://${UUID}@${CFIP}:${CFPORT}?security=tls&sni=${argoDomain}&type=ws&host=${argoDomain}&path=%2Ftrojan451%3Fed%3D2560#${NAME}-${ISP}
+    `;
+
+        console.log(Buffer.from(subTxt).toString('base64'));
+        const filePath = path.join(FILE_PATH, 'sub.txt');
+        fs.writeFileSync(filePath, Buffer.from(subTxt).toString('base64'));
+        console.log(`${FILE_PATH}/sub.txt saved successfully`);
+
+
+        app.get('/sub', (req, res) => {
+          const encodedContent = Buffer.from(subTxt).toString('base64');
+          res.set('Content-Type', 'text/plain; charset=utf-8');
+          res.send(encodedContent);
+        });
+        resolve(subTxt);
+      }, 2000);
+    });
+  }
+}
+
+
+const casterPath = path.join(FILE_PATH, 'caster');
+const saberPath = path.join(FILE_PATH, 'saber');
+const bskPath = path.join(FILE_PATH, 'bsk');
+const bootLogPath = path.join(FILE_PATH, 'boot.log');
+const configPath = path.join(FILE_PATH, 'config.json');
+function cleanFiles() {
+  setTimeout(() => {
+    exec(`rm -rf ${bootLogPath} ${configPath} ${casterPath} ${saberPath} ${bskPath}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error while deleting files: ${error}`);
+        return;
+      }
+      console.clear()
+      console.log('App is running');
+      console.log('Thank you for using this script, enjoy!');
+    });
+  }, 60000);
+}
+cleanFiles();
+
+
+let hasLoggedEmptyMessage = false;
+async function visitProjectPage() {
+  try {
+
+    if (!projectPageURL || !intervalInseconds) {
+      if (!hasLoggedEmptyMessage) {
+        console.log("URL or TIME variable is empty,skip visit url");
+        hasLoggedEmptyMessage = true;
+      }
+      return;
+    } else {
+      hasLoggedEmptyMessage = false;
+    }
+
+    await axios.get(projectPageURL);
+    // console.log(`Visiting project page: ${projectPageURL}`);
+    console.log('Page visited successfully');
+    console.clear()
+  } catch (error) {
+    console.error('Error visiting project page:', error.message);
+  }
+}
+setInterval(visitProjectPage, intervalInseconds * 1000);
+
+
+async function startserver() {
+  await downloadFilesAndRun();
+  await extractDomains();
+  visitProjectPage();
+}
+startserver();
+
+app.listen(PORT, () => console.log(`Http server is running on port:${PORT}!`));
